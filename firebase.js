0f8790ece6b93dd201158f67410bef6586fe8266
roan1088 <rohan_188@yahoo.com>
2025-05-23 20:17:26 -0400

    Added storing count to firebase database

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBD6RW9eNHEp6JqcqUi-CbcAzFsnQIjrE",
+  authDomain: "webdev-final-e9788.firebaseapp.com",
+  projectId: "webdev-final-e9788",
+  storageBucket: "webdev-final-e9788.firebasestorage.app",
+  messagingSenderId: "97543641556",
+  appId: "1:97543641556:web:f6d41918e724eab4629e41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { db } from '../firebase'
+import { addDoc, collection, getDocs, query, updateDoc, where } from 'firebase/firestore'
+
 function App() {
   const [count, setCount] = useState(0)
+  const [doc, setDoc] = useState(null)
+
+  useEffect(() => {
+    const loadCollection = async () => {
+      const q = query(collection(db, 'test'), where("uid", "==", "1234"))
+      const snap = await getDocs(q);
+
+      if (snap.empty) {
+        console.log("No documents found")
+      } else {
+        const firstDoc = snap.docs[0]
+        // console.log(firstDoc)
+        setDoc(firstDoc)
+        setCount(firstDoc.data().count)
+      }
+    }
+
+    loadCollection()
+  }, [])
+
+  useEffect(() => {
+    if (count == 0) {
+      return
+    }
+
+    console.log("count changed to " + count)
+    if (!doc) {
+      const newDoc = addDoc(collection(db, "test"), {
+        uid: "1234",
+        count
+      })
+      setDoc(newDoc)
+    } else {
+      const result = updateDoc(doc.ref, {count})
+    }
+  }, [count])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Counter App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click on the button to increment count
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
